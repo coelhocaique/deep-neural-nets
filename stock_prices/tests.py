250dc1558a9a2fddf80f1b1066810439b2c64033
Caique dos Santos Coelho <ccoelho@eicon.com.br>
2017-08-02 16:21:47 -0300

    input shape

--- stock_prices/tests.py
+++ stock_prices/tests.py
@@ -17,18 +17,30 @@ standard_deviation =  np.std(returns)
 returns = [(ret - mean) / standard_deviation for ret in returns]
 labels = []
 sequences = []
+batch_size = 0
 for i in range(0,len(returns) - 2):
     try:
-        new_returns = np.array(returns[i:239 + i])
+        new_returns = np.array(returns[i:240 + i])
         mean = np.mean(new_returns)
-        if returns[239 + i] > mean: label = 1
+        if returns[240 + i] > mean: label = 1
         else: label = -1
         labels.append(label)
         sequences.append(new_returns)
-        t_plus_1 = returns[239 + i]
-        print "Sequence ",i ,"\nmean :",mean,"\nlabel(t+1): ",label,t_plus_1,"\n","subtracted: ",(t_plus_1 - mean)
-        print "real profit: ",profit[239 + i],"\n"
+        t_plus_1 = returns[240 + i]
+        #print "Sequence ",i ,"\nmean :",mean,"\nlabel(t+1): ",label,t_plus_1,"\n","subtracted: ",(t_plus_1 - mean)
+        #print "real profit: ",profit[239 + i],"\n"
     except IndexError:
+        batch_size = i
         break
 
 print len(labels) == len(sequences)
+print batch_size
+#print sequences[0]
+input_data = np.zeros((batch_size,240,1))
+#input_data[0] = sequences[0]
+print input_data.shape
+print len(input_data[0])
+print input_data[0].shape
+for i in range(len(sequences)):
+    input_data[i] = sequences[i].reshape(len(sequences[i]),1)
+print input_data[1759],labels[1759]
