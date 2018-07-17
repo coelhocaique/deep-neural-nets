5171c06b85b99ac2b889ebc6cf535272afbed6a0
coelhocaique <coelhocaiquesantos@hotmail.com>
2018-07-17 12:16:52 -0300

    logistic-regression: restructuring

--- logistic-regression/logistic_regression.py
+++ logistic-regression/logistic_regression.py
@@ -1,76 +1,87 @@
 import numpy as np
-import h5py
 
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+class LogisticRegresion:
 
-def propagate(w, b , X, Y):
+    def __init__(self, dim):
+        self.w = np.zeros((dim, 1))
+        self.b = 0
 
-    m = X.shape[1]
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
 
-    Z = np.dot(w.T, X) + b
+    def compute_cost(self, m ,Y , A):
+        return (-1/m) * np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1 - A)))
 
-    A = sigmoid(Z)
+    def forward_pass(self, X):
+        Z = np.dot(self.w.T, X) + self.b
 
-    cost = (-1/m) * np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1 - A)))
+        return self.sigmoid(Z)
 
-    dw = (1/m) * np.dot(X, (A - Y).T)
-    db = (1/m) * np.sum(A - Y)
+    def backward_pass(self, m ,X, A, Y):
+        dw = (1/m) * np.dot(X, (A - Y).T)
+        db = (1/m) * np.sum(A - Y)
 
-    return dw, db, cost
+        return dw, db
 
-def optimize(w, b, X, Y, num_iterations, learning_rate = 0.01, print_cost = False):
+    def propagate(self,X, Y):
+        m = X.shape[1]
+        A = self.forward_pass(X)
+        cost = self.compute_cost(m ,Y , A)
+        dw, db = self.backward_pass(m ,X, A, Y)
 
-    costs = []
+        return dw, db, cost
 
-    for i in range(num_iterations + 1):
+    def optimize(self, X, Y, num_iterations, learning_rate , print_cost):
 
-        dw, db, cost = propagate(w, b, X, Y)
+        costs = []
 
-        w = w - learning_rate * dw
-        b = b - learning_rate * db
+        for i in range(num_iterations + 1):
 
-        if print_cost and i % 100 == 0:
-            print("Cost after iteration %i: %f " % (i, cost))
+            dw, db, cost = self.propagate(X, Y)
 
-        costs.append(cost)
+            self.w = self.w - learning_rate * dw
+            self.b = self.b - learning_rate * db
 
-    return w, b , dw, db, costs
+            if print_cost and i % 100 == 0:
+                print("Cost after iteration %i: %f " % (i, cost))
 
+            costs.append(cost)
 
-def predict(w, b, X):
-
-    m = X.shape[1]
-    predictions = np.zeros((1, m))
-    w = w.reshape(X.shape[0], 1)
-
-    Z = np.dot(w.T, X) + b
-    A = sigmoid(Z)
-
-    for i in range(A.shape[1]):
-        predictions[0, i] = 0 if A[0, i] <= 0.5 else 1
-
-    return predictions
+        return dw, db, costs
 
 
-def model(x_train, y_train,
-          x_test, y_test,
-          num_iterations = 2000, learning_rate = 0.01, print_cost = False):
+    def predict(self, X):
 
-    w, b = np.zeros((x_train.shape[0], 1)), 0
+        m = X.shape[1]
+        predictions = np.zeros((1, m))
+        self.w = self.w.reshape(X.shape[0], 1)
 
-    w, b , dw, db, costs = optimize(w, b, x_train, y_train, num_iterations, learning_rate, print_cost)
+        A = self.forward_pass(X)
 
-    prediction_test = predict(w, b, x_test)
-    prediction_train = predict(w, b, x_train)
+        for i in range(A.shape[1]):
+            predictions[0, i] = 0 if A[0, i] <= 0.5 else 1
 
-    return prediction_test, prediction_train, costs
+        return predictions
 
-def evaluate(y_label, y_prediction):
-    m_test = y_prediction.shape[1]
-    corrects = 0
-    for i in range(m_test):
-        if y_prediction[0,i] == y_label[0, i]:
-            corrects+= 1
 
-    return (corrects * 100 ) / m_test
+    def fit(self, x_train, y_train,
+            x_test, y_test,
+            num_iterations = 2000,
+            learning_rate = 0.01,
+            print_cost = False):
+
+        dw, db, costs = self.optimize(x_train, y_train, num_iterations, learning_rate, print_cost)
+
+        prediction_test = self.predict(x_test)
+        prediction_train = self.predict(x_train)
+
+        return prediction_train, prediction_test, costs
+
+    def evaluate(self, y_label, y_prediction):
+        m_test = y_prediction.shape[1]
+        corrects = 0
+        for i in range(m_test):
+            if y_prediction[0,i] == y_label[0, i]:
+                corrects+= 1
+
+        return (corrects * 100 ) / m_test
--- logistic-regression/runner.py
+++ logistic-regression/runner.py
@@ -1,8 +1,16 @@
 from dataset_loader import load_dataset, flatten_dataset
-from logistic_regression import model, evaluate
+from logistic_regression import LogisticRegresion
 
 x_train, y_train, x_test, y_test = load_dataset()
 x_train, x_test = flatten_dataset(x_train, x_test)
-prediction_test, prediction_train, costs = model(x_train, y_train, x_test, y_test,2000,0.0001,print_cost = True)
-print('Train accuracy : %f' % evaluate(y_train, prediction_train))
-print('Test accuracy : %f' % evaluate(y_train, prediction_test))
+
+model = LogisticRegresion(x_train.shape[0])
+
+prediction_train, prediction_test, costs = model.fit(x_train, y_train,
+                                                   x_test, y_test,
+                                                   num_iterations = 20000,
+                                                   learning_rate = 0.01,
+                                                   print_cost = True)
+
+print('Train accuracy : %f' % model.evaluate(y_train, prediction_train))
+print('Test accuracy : %f' % model.evaluate(y_test, prediction_test))
